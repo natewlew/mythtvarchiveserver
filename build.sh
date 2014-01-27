74714aa65b717361a5b8611c5da04eb47a65094e
Nathan Lewis <natewlew@gmail.com>
2014-01-26 22:56:58 -0600

    Updated build script

--- build.sh
+++ build.sh
@@ -3,20 +3,27 @@
 
 function build_deb {
 
-    python setup.py --command-packages=stdeb.command sdist_dsc
-    cp deb_dist/*.orig.tar.gz ../
-    rm -rf deb_dist/
+    #python setup.py --command-packages=stdeb.command sdist_dsc
+    #cp deb_dist/*.orig.tar.gz ../
+    #rm -rf deb_dist/
     dpkg-buildpackage
 
 }
 
 function clean {
 
+    #rm -rf deb_dist/
     rm -rf build/
-    rm -rf deb_dist/
     rm -rf ./*.egg-info/
     find . -name '*.pyc' -delete
     rm -rf ../mythtvarchiveserver_*
+    rm -rf ../mythtvarchiveservermedia_*
+
+    rm -rf debian/mythtvarchiveserver/
+    rm -rf debian/mythtvarchiveservermedia/
+    rm -rf debian/tmp/
+    rm -rf debian/*debhelper*
+    rm -rf debian/*substvars
 
 }
 
