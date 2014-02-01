6a17e8c3ce7618d8fe4ead167919e693d6f7bd9f
Nathan Lewis <natewlew@gmail.com>
2014-02-01 16:02:13 -0600

    Fixed Build Errors

--- setup.py
+++ setup.py
@@ -5,25 +5,30 @@
 
 """
 
+__version__ = '0.1'
+__author__ = 'Nathan Lewis'
+__email__ = 'natewlew@gmail.com'
+__license__ = 'GPL Version 2'
+
+
 try:
     import twisted
 except ImportError:
     raise SystemExit("twisted not found.  Make sure you "
                      "have installed the Twisted core package.")
 
-
 #python-sqlalchemy, python-twisted
-
 from setuptools import setup
 
 setup(
     name = "MythTVArchiveServer",
-    version = "0.1",
-    author = "Nathan Lewis",
-    author_email = "natewlew@gmail.com",
-    license = "GPL Version 2",
+    version = __version__,
+    author = __author__,
+    author_email = __email__,
+    license = __license__,
     packages=['MythTVArchiveServer', 'MythTVArchiveServer.controllers', 'MythTVArchiveServer.lib',
-              'MythTVArchiveServer.models', 'MythTVArchiveServer.util', 'twisted.plugins',],
+              'MythTVArchiveServer.models', 'MythTVArchiveServer.util', 'MythTVArchiveServer.resource',
+              'twisted.plugins',],
     package_data={
             'twisted': ['plugins/mythtvarchiveserver_plugin.py',
                         'plugins/mythtvarchiveserver_media_plugin.py'],
